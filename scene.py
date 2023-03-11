d918a87ff8c61feb4bc737df547daaa5140359ae
Markus <markus.storeide@gmail.com>
2023-03-11 19:22:17 +0100

    Name Update

--- scene.py
+++ scene.py
@@ -1,9 +1,10 @@
 #Phong Model
 
-import manim
+from manim import * 
 
 class PhongBRDFModel(Scene):
     def construct(self):
         formula = MathTex(r"fr,s(wi, wr)")
         self.play(Write(formula), run_time=3)
         self.wait
+        
