52d9fec7889fdb6c6db2ea73ff51c4fb3d27d040
Markus <markus.storeide@gmail.com>
2023-03-14 22:29:23 +0100

    CG Text

--- Radiometry.py
+++ Radiometry.py
@@ -10,13 +10,17 @@ class Radiometry(Scene):
         colorlab.scale(2)
         colorlabtext = Text("Colourlab").move_to([-1,0,0])
         colorlabtext.scale(2)
+        cg = Text("IDIG4002 - Computer Graphics").move_to([0,-1,0])
+        cg.scale(0.5)
+
+        #ntnu = ImageMobject(NTNULOGO)
 
         self.wait()
 
-        self.play(FadeIn(colorlab, shift=RIGHT*2), Write(colorlabtext, shift=LEFT*2))
+        self.play(FadeIn(colorlab, shift=RIGHT*2), Write(colorlabtext, shift=LEFT*2), Write(cg, run_time=0.7))
         self.wait(1)
 
-        self.play(FadeOut(colorlab, shift=LEFT), Unwrite(colorlabtext, shift=RIGHT))
+        self.play(FadeOut(colorlab, shift=LEFT), Unwrite(colorlabtext, shift=RIGHT), Unwrite(cg, run_time=0.7))
 
         self.wait()
 
